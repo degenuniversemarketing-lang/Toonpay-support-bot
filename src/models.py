efa96e4b88b5d558827082289845563dbed73c49
degen <degenuniversemarketing@gmail.com>
2026-03-11 04:04:49 +0500

    Update models.py

--- src/models.py
+++ src/models.py
@@ -1,1 +1,6 @@
+from database import Base
+from sqlalchemy import Column, Integer, String, DateTime, Text, BigInteger, Boolean, ForeignKey
+from datetime import datetime
 
+# Models are already in database.py, this file can be used for additional models
+# or you can move all models here and import them
