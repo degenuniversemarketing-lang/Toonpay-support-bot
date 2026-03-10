b6cfedffa645c60a47cb8a3a8c443a705a4175d5
degen <degenuniversemarketing@gmail.com>
2026-03-11 04:42:39 +0500

    Update config.py

--- src/config.py
+++ src/config.py
@@ -6,8 +6,8 @@ load_dotenv()
 class Config:
     BOT_TOKEN = os.getenv('BOT_TOKEN')
     DATABASE_URL = os.getenv('DATABASE_URL')
-    SUPER_ADMIN_ID = int(os.getenv('SUPER_ADMIN_ID', 0))
-    ADMIN_GROUP_ID = int(os.getenv('ADMIN_GROUP_ID', 0))
+    SUPER_ADMIN_ID = int(os.getenv('SUPER_ADMIN_ID', '0'))
+    ADMIN_GROUP_ID = int(os.getenv('ADMIN_GROUP_ID', '0'))
     
     # Categories for tickets
     CATEGORIES = [
