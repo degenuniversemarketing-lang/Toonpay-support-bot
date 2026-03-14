c14aa072ccb483c85d9a70dc35f0e7396a021075
degen <degenuniversemarketing@gmail.com>
2026-03-14 15:03:56 +0500

    Update __init__.py

--- src/handlers/__init__.py
+++ src/handlers/__init__.py
@@ -1,6 +1,1 @@
-from . import user
-from . import admin
-from . import super_admin
-from . import group
-
-__all__ = ['user', 'admin', 'super_admin', 'group']
+# Empty file to make handlers a package
